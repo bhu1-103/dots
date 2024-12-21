51b11b07deb3cfab878367e201d34f5366783f16
bhu1-103 <103.bhuvan@gmail.com>
2024-12-21 23:56:21 +0530

    better script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/zsh
 
-cd dotfiles
-cp -r bspwm ~/.config/bspwm
-cp -r polybar ~/.config/polybar
-cp -r hypr ~/.config/hypr
-cp -r i3 ~/.config/hypr
+if pacman -Q bspwm &>/dev/null; then
+    echo "bspwm found"
+    cp -r dotfiles/bspwm ~/.config/bspwm
+fi
+
+if pacman -Q hyprland-git &>/dev/null; then
+    echo "hyprland-git found"
+    cp -r dotfiles/hypr ~/.config/hypr
+fi
+
+if pacman -Q i3-wm &>/dev/null; then
+    echo "i3-wm found"
+    cp -r dotfiles/i3 ~/.config/i3
+fi
+
+if pacman -Q polybar &>/dev/null; then
+    echo "polybar found"
+    cp -r dotfiles/polybar ~/.config/polybar
+fi
+
+echo "rice done"
